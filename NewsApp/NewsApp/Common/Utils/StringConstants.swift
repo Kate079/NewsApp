d9aea21d775ccc8fd8778fc82ddce8386bc35158
Kate <adamova.dev@gmail.com>
2022-11-21 22:50:17 +0200

    Fixed api key

--- NewsApp/NewsApp/Common/Utils/StringConstants.swift
+++ NewsApp/NewsApp/Common/Utils/StringConstants.swift
@@ -8,11 +8,7 @@
 import Foundation
 
 enum StringConstants {
-//    static let apiKey = "8ba6f5d10a7a4764927915040cc410fc"
-
-//    static let apiKey = "d412547cc2a14d5aa893686b27b0c95b"
-
-    static let apiKey = "c51fd47e4bb34927a2d07af85221a0d1"
+    static let apiKey = "8ba6f5d10a7a4764927915040cc410fc"
 
     static let errorTitle = "Помилка"
     static let okTitle = "OK"
